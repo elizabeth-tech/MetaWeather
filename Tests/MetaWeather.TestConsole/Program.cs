34a80d21598d639df1c59255fdb13afedae9a477
elizabeth-tech <elizabeth-tech@gmail.com>
2021-07-21 12:08:51 +0700

    Создание хоста приложения в тестовом консольном проекте

--- Tests/MetaWeather.TestConsole/Program.cs
+++ Tests/MetaWeather.TestConsole/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace MetaWeather.TestConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IHost hosting;
+
+        public static IHost Hosting => hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        // Создаем хост приложения и конфигурируем сервисы
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(ConfigureServices);
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+        static async Task Main(string[] args)
+        {
+            using var host = Hosting;
+            await host.StartAsync();
+
+            Console.WriteLine("Completed successfully!");
+            Console.ReadKey();
+            await host.StopAsync();
         }
     }
 }
